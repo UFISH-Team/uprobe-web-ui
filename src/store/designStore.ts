bc6ac6961f371b345a11a6c8956fec0e308bda37
qzhang <105474865+jshn2022@users.noreply.github.com>
2025-08-26 22:23:22 +0800

    Update designStore.ts

--- src/store/designStore.ts
+++ src/store/designStore.ts
@@ -3,41 +3,21 @@ import ApiService from '../api';
 import { CustomProbeType} from '../types';
 
 
-interface Gene {
-  gene: string;
-  [key: string]: string; // Allow dynamic barcode fields
-}
-
-interface Pool {
-  name: string;
-  location: string;
-  numbers: number;
-  density: number;
+interface Target {
+  target: string;
   [key: string]: string | number;  // Allow dynamic barcode fields
 }
 
 interface DesignState {
   // Basic task info
   taskName: string;
-  probeType: 'RCA' | 'DNA-FISH' | string;
+  probeType: string;
   species: string;
   
-  // RCA specific
-  geneList: Gene[];
+  // Unified target configuration
+  targetList: Target[];
   minLength: number;
   overlap: number;
-  partLengths: {
-    part1: number;
-    part2: number;
-    part3: number;
-  };
-  
-  // DNA-FISH specific
-  dnaFishParams: {
-    length: number;
-    overlap: number;
-    poolList: Pool[];
-  };
 
   // Custom probe type specific
   selectedCustomType: CustomProbeType | null;
@@ -53,24 +33,17 @@ interface DesignState {
   
   // Actions
   setTaskName: (name: string) => void;
-  setProbeType: (type: 'RCA' | 'DNA-FISH' | string) => void;
+  setProbeType: (type: string) => void;
   setSpecies: (species: string) => void;
   setSelectedCustomType: (type: CustomProbeType | null) => void;
   
-  // RCA actions
-  setGeneList: (list: Gene[]) => void;
-  addGene: () => void;
-  removeGene: (index: number) => void;
-  updateGene: (index: number, field: keyof Gene, value: string) => void;
+  // Unified target actions
+  setTargetList: (list: Target[]) => void;
+  addTarget: () => void;
+  removeTarget: (index: number) => void;
+  updateTarget: (index: number, field: keyof Target, value: string | number) => void;
   setMinLength: (length: number) => void;
   setOverlap: (overlap: number) => void;
-  setPartLengths: (part: 'part1' | 'part2' | 'part3', length: number) => void;
-  
-  // DNA-FISH actions
-  setDnaFishParams: (params: Partial<DesignState['dnaFishParams']>) => void;
-  addPool: () => void;
-  removePool: (index: number) => void;
-  updatePool: (index: number, field: string, value: string | number) => void;
   
   // UI actions
   setSubmitting: (isSubmitting: boolean) => void;
@@ -86,17 +59,11 @@ interface DesignState {
 const useDesignStore = create<DesignState>((set, get) => ({
   // Initial state
   taskName: '',
-  probeType: 'RCA',
+  probeType: '',
   species: '',
-  geneList: [{ gene: '' }],
+  targetList: [{ target: '' }],
   minLength: 40,
   overlap: 20,
-  partLengths: { part1: 13, part2: 13, part3: 13 },
-  dnaFishParams: {
-    length: 70,
-    overlap: 20,
-    poolList: [{ name: '', location: '', numbers: 0, density: 0}],
-  },
   selectedCustomType: null,
   isSubmitting: false,
   progress: 0,
@@ -112,52 +79,21 @@ const useDesignStore = create<DesignState>((set, get) => ({
   setSpecies: (species) => set({ species }),
   setSelectedCustomType: (type) => set({ selectedCustomType: type }),
   
-  // RCA setters
-  setGeneList: (list) => set({ geneList: list }),
-  addGene: () => set((state) => ({
-    geneList: [...state.geneList, { gene: '' }],
+  // Unified target setters
+  setTargetList: (list) => set({ targetList: list }),
+  addTarget: () => set((state) => ({
+    targetList: [...state.targetList, { target: '' }],
   })),
-  removeGene: (index) => set((state) => ({
-    geneList: state.geneList.filter((_, i) => i !== index),
+  removeTarget: (index) => set((state) => ({
+    targetList: state.targetList.filter((_, i) => i !== index),
   })),
-  updateGene: (index, field, value) => set((state) => ({
-    geneList: state.geneList.map((gene, i) =>
-      i === index ? { ...gene, [field]: value } : gene
+  updateTarget: (index, field, value) => set((state) => ({
+    targetList: state.targetList.map((target, i) =>
+      i === index ? { ...target, [field]: value } : target
     ),
   })),
   setMinLength: (length) => set({ minLength: length }),
   setOverlap: (overlap) => set({ overlap }),
-  setPartLengths: (part, length) => set((state) => ({
-    partLengths: { ...state.partLengths, [part]: length },
-  })),
-  
-  // DNA-FISH setters
-  setDnaFishParams: (params) => set((state) => ({
-    dnaFishParams: { ...state.dnaFishParams, ...params },
-  })),
-  addPool: () => set((state) => ({
-    dnaFishParams: {
-      ...state.dnaFishParams,
-      poolList: [
-        ...state.dnaFishParams.poolList,
-        { name: '', location: '', numbers: 0, density: 0},
-      ],
-    },
-  })),
-  removePool: (index) => set((state) => ({
-    dnaFishParams: {
-      ...state.dnaFishParams,
-      poolList: state.dnaFishParams.poolList.filter((_, i) => i !== index),
-    },
-  })),
-  updatePool: (index, field, value) => set((state) => ({
-    dnaFishParams: {
-      ...state.dnaFishParams,
-      poolList: state.dnaFishParams.poolList.map((pool, i) =>
-        i === index ? { ...pool, [field]: value } : pool
-      ),
-    },
-  })),
   
   // UI setters
   setSubmitting: (isSubmitting) => set({ isSubmitting }),
